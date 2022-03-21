d2128bb1945b3b9778f8361c2edc7df2fe87e505
Alex Zhd <my_email@test.com>
2022-03-21 22:10:30 +0700

    добавил документацию API

--- src/main/java/com/example/demo/controller/GroupController.java
+++ src/main/java/com/example/demo/controller/GroupController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.GroupDto;
 import com.example.demo.service.GroupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/group")
+@Tag(name = "Группы")
 public class GroupController {
 
     private final GroupService groupService;
@@ -22,18 +25,21 @@ public class GroupController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Получить страницу с группами")
     public Page<GroupDto> findPage(@RequestParam("pageIndex") Integer pageIndex) {
         return groupService.findPage(pageIndex);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Получить группу по id")
     public GroupDto findById(@PathVariable("id") Long id) {
         return groupService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Добавить группу")
     public GroupDto save(@RequestBody @Valid GroupDto groupDto) {
         return groupService.save(groupDto);
     }
--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.StudentDto;
 import com.example.demo.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/student")
+@Tag(name = "Студенты")
 public class StudentController {
 
     private final StudentService studentService;
@@ -21,12 +24,14 @@ public class StudentController {
 
     @PostMapping("/{groupId}")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Добавить студента")
     public StudentDto save(@RequestBody @Valid StudentDto studentDto, @PathVariable("groupId") Long groupId) {
         return studentService.save(studentDto, groupId);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Удалить студента")
     public void delete(@PathVariable("id") Long id) {
         studentService.delete(id);
     }
